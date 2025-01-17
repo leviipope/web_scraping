d8c237be9e7ab7709502bc39f4a352ab580d70fd
leviipope <150926233+leviipope@users.noreply.github.com>
2025-01-17 15:21:22 +0100

    second

--- program.py
+++ program.py
@@ -5,10 +5,10 @@ from soupsieve.pretty import pretty
 url = "https://hardverapro.hu/apro/7j8rt/friss.html"
 
 result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+doc = BeautifulSoup(result.text, "html.parser") # process the url as a html
 
-price = doc.find_all("h2") # print(price)
-# parent = price[0].parent
-price_with_ft = price[0].string
-forint =  int(re.sub(r'\D', '',price_with_ft))
+price = doc.find_all("h2") # an array of finds of the h2 tags
+# parent = price[0].parent # finds the parent of price[0] (= 359 990 Ft)
+price_with_ft = price[0].string # get the value which is between the tags
+forint = int(price_with_ft.replace(" Ft","").replace(" ", ""))
 print(price_with_ft, "->", forint)
---
+++ second_part.py
@@ -0,0 +1,31 @@
+# searching for tags, tag attributes, find multiple tags, find attributes, find class name, find regex, find limit, save modified html
+
+from bs4 import BeautifulSoup
+import re
+
+with open("index.html", "r") as f: # opening local html file
+	doc = BeautifulSoup(f, "html.parser")
+
+# Basic usage of tags
+
+result99 = doc.find("option") # the first find
+result98 = doc.find_all("option") # array with all the finds
+
+tag = doc.find("option") # finds the first option tag
+tag["value"] = "new value" # changes the value attr to 'new value'
+tag["color"] = "blue" # add a new attr
+# print(tag.attrs) # all the attrs of tag
+
+tags = doc.find_all(["p", "div", "li"]) # searching for multiple tags
+tags2 = doc.find_all(["option"], string="Undergraduate", value="undergraduate") # option tag with the content of "Undergraduate" and the value attr set to "undergraduate"
+tags3 = doc.find_all(class_="btn-item") # searching for class names
+tags4 = doc.find_all(string=re.compile(r"\$.*"), limit = 1) # using regex and limit
+
+# Saving the modified HTML
+
+tags0 = doc.find_all("input", type="text")
+for tag in tags0:
+	tag['placeholder'] = "I changed you!"
+
+with open("changed.html", "w") as file: # w mode creates or overrides
+	file.write(str(doc))
